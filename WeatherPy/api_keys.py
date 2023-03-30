3989fa6170a409ccbb1b94d837142190c028dc7f
PriscilaGlienke <priglienke@gmail.com>
2023-03-29 21:23:42 -0400

    changes

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "b96b43a9a30e4f8abbf8093d1d00858a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a0532b0749bc4fd0a0b0e1cbd3d580f7"
